29a41540ff36121e833cda269d23d7102dfc93f9
Bananaz21 <72627687+Bananaz21@users.noreply.github.com>
2021-08-18 23:08:58 -0400

    forgor

--- ChatGUI-v1.py
+++ ChatGUI-v1.py
@@ -6,7 +6,7 @@ import urllib.parse
 import json
 
 sio = socketio.Client()
-url = 'http://localhost:80'
+url = input('What is the ip? Remember to add the "http" as well!')
 s = requests.Session()
 
 sg.theme('LightGreen1') # give our window a spiffy set of colors
